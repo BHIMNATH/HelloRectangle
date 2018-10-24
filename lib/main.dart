dd680b4f482db828537892d2667c539dc99634d3
Aswin Bhim Nath <aswismail@gmail.com>
2018-10-24 09:33:38 +0530

    Setting Properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
 
-class HelloRectangle extends StatelessWidget {}
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Container(
+        color: Colors.red,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'Hello',
+            style: TextStyle(fontSize: 40.0),
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+      ),
+    );
+  }
+}
+}
